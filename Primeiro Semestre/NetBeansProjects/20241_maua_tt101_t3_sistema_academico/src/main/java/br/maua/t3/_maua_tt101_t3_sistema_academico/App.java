f7ab39faf372bd6dab13ed37a2f5ab84538a6f05
GUILHERME ALMEIDA AMORIM DA SILVA <24.00931-8@maua.br>
2024-04-15 11:26:01 -0300

    feat(project): creating a project in NetBeans and adding files to POO

---
+++ Primeiro Semestre/NetBeansProjects/20241_maua_tt101_t3_sistema_academico/src/main/java/br/maua/t3/_maua_tt101_t3_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua.t3._maua_tt101_t3_sistema_academico;
+
+/**
+ *
+ * @author 24.00931-8
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- Primeiro Semestre/POO/oo_parte2/Personagem.java
+++ Primeiro Semestre/POO/oo_parte2/Personagem.java
@@ -1,43 +1,62 @@
-public class Personagem {
+public class Personagem{
+  
+  //variáveis de instância(instância é sinônimo de objeto)
+  String nome;
+  private int energia = 10, fome = 0, sono = 0;
+  // private int energia = 10;
+  // private int fome = 0;
+  // private int sono = 0;
 
-    // váriaveis de (insância é sinônimo de objeto)
-    String nome;
-    private int energia = 10, fome = 0, sono = 0;
-
-    void cacar() {
-        if (energia >= 2) {
-            System.out.println(nome + " Caçando");
-            energia = energia - 2; // energia -= 2;
-        } else {
-            System.out.printf("%s você não tem energia suficiente para caçar\n", nome);
-            fome = Math.min(fome + 1, 10);
-            sono = sono == 10 ? sono : sono + 1;
-        }
-
+  //método de instância
+  void cacar(){
+    if(energia >= 2){
+      System.out.println(nome + " caçando");
+      energia = energia - 2; // energia -= 2;
     }
-
-    void dormir() {
-        if (sono >= 1) {
-            System.out.println(nome + " Dormindo...");
-            sono--; // --sono; //sono = sono -1; // sono -= 1;
-            energia = Math.min(energia + 1, 10);
-        } else {
-            System.out.println(nome + "sem sono");
-        }
+    else{
+      System.out.printf("%s sem energia para caçar\n", nome);
     }
-
-    // método de instânciar
-    void comer() {
-        // lógica em queda (fall-through)
-        switch (fome) {
-            case 0:
-                System.out.println(nome + "sem fome");
-                break;
-            default:
-                System.out.println(nome + "comendo");
-                --fome;
-        }
-
+    fome = Math.min(fome + 1, 10);
+    sono = sono == 10 ? sono : sono + 1;
+  }
+  // método de instância
+  void dormir(){
+    if(sono >= 1){
+      System.out.println(nome + " dormindo");
+      sono = sono - 1;// sono -= 1;// --sono;// sono--;
+      energia = Math.min(energia + 1, 10);
     }
+    else{
+      System.out.println(nome + " sem sono");
+    }
+  } 
+  // método de instância
+  void comer(){
+    //lógica em queda (fall-through)
+    switch(fome){
+      case 0:
+        System.out.println(nome + " sem fome");
+        break;
+      default:
+        System.out.println(nome + " comendo");
+        --fome;
+        energia = energia < 10 ? energia + 1 : energia;
+    }
+  }
 
+  public void setEnergia(int energia){
+    if(energia >= 0 && energia <= 10)
+      this.energia = energia;
+  }
+
+  public int getEnergia(){
+    return energia;
+  }
+
+  public String toString(){
+    return String.format(
+      "Energia: %d, Fome: %d, Sono: %d",
+      energia, fome, sono
+    );
+  }
 }
